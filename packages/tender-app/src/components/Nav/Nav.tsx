75323110c7d832af2c615cc0e7d3f7524a98e94e
Kevin Weaver <kevinweaver2@gmail.com>
2020-10-20 23:31:40 -0400

    Add homepage link to logo

--- packages/tender-app/src/components/Nav/Nav.tsx
+++ packages/tender-app/src/components/Nav/Nav.tsx
@@ -1,40 +1,42 @@
-import React from 'react'
+import React from "react";
 import { ConnectButton } from "..";
-import Address from "../Address/Address"
-import { Row, Col } from "react-bootstrap"
+import Address from "../Address/Address";
+import { Link } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
 
 export default class Nav extends React.Component<any> {
-
-  constructor(props:any) {
-    super(props)
+  constructor(props: any) {
+    super(props);
   }
 
   render() {
-    const logo = require("../../img/tenderizeLogo.svg")
+    const logo = require("../../img/tenderizeLogo.svg");
 
-    let component 
+    let component;
     if (this.props.cachedProvider) {
       component = (
-        <Col md={{span:3, offset: 6}}>
+        <Col md={{ span: 3, offset: 6 }}>
           <Address address={this.props.address} />
         </Col>
-      )
+      );
     } else {
       component = (
-        <Col md={{ span:3, offset: 6}}>
-        <ConnectButton onConnect={this.props.onConnect} />
+        <Col md={{ span: 3, offset: 6 }}>
+          <ConnectButton onConnect={this.props.onConnect} />
         </Col>
-      )
+      );
     }
-    return(
+    return (
       <>
         <Row>
-        <Col md={{ span: 3 }} lg={{ span:3 }}>
-          <img src={logo} alt="logo" style={{marginTop: "1em"}}/>
-        </Col>
+          <Col md={{ span: 3 }} lg={{ span: 3 }}>
+            <Link to="/">
+              <img src={logo} alt="logo" style={{ marginTop: "1em" }} />
+            </Link>
+          </Col>
           {component}
         </Row>
       </>
     );
   }
- }
+}
